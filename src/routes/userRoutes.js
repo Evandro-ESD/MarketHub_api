dc4e830e881cd88311d67a9f172f678d892767f0
Fabio <fabioluizrl2@gmail.com>
2025-08-28 11:09:05 -0300

    teste

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 const userController = require('../controllers/userController');
-
+const { verifyToken, verifyPerfil } = require('../middlewares/authMiddleware');
 // Buscar todos os usuários
 router.get('/', userController.getAllUsers);
 
@@ -15,5 +15,16 @@ router.put('/:id_usuario', userController.updateUser);
 
 // Excluir usuário
 router.delete('/:id_usuario', userController.deleteUser);
+
+
+
+// Proteger todas as rotas (usuário precisa estar autenticado)
+router.use(verifyToken);
+
+// Exemplo: rota que só permite perfil 'VENDEDOR'
+router.post('/', verifyPerfil('VENDEDOR'), userController.createUser);
+
+// Exemplo: rota que só permite perfil 'COMPRADOR'
+router.post('/', verifyPerfil('COMPRADOR'), userController.createUser);
 
 module.exports = router;
